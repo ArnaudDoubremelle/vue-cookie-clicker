6c69a173103b2d7330de1cabf1f5abf2f010e349
Arnaud Doubremelle <a-r-n-a-u-d-@hotmail.fr>
2018-07-24 10:08:45 +0200

    add Cookie component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Cookie></Cookie>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import Cookie from './components/Cookie.vue';
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: 'app',
+        components: {
+            Cookie
+        }
+    };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html, body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 100%;
+    }
 </style>
---
+++ src/components/Cookie.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="content">
+        <div class="cookie-block">
+            <img class="cookie" src="../assets/cookie.png" alt="cookie">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Cookie.vue'
+    };
+</script>
+
+<style scoped>
+    .content {
+        display: flex;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .cookie {
+        cursor: pointer;
+    }
+</style>
